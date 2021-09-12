d1e6f79c5f11459183533e1dc52f17d3e002e90b
Martin Sobarzo <msobarzo@utem.cl>
2021-09-12 20:45:05 -0300

    se agregaron algunos detalles

--- Service/src/routes/index.js
+++ Service/src/routes/index.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.json({ "Title": "Hola Chicos" });
+    res.json({ "Title": "Hola Chicxs" });
 });
 
 module.exports = router;
